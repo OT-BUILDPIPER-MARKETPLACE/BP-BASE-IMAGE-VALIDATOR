be12a38257944c9323294a4596544de1e384d423
Rishabh <rishabhbohra01@gmail.com>
2023-10-12 20:59:30 +0530

    enable whitelisting multiple images

--- build.sh
+++ build.sh
@@ -11,7 +11,6 @@ logInfoMessage "I'll do processing at [$CODEBASE_LOCATION]"
 sleep  $SLEEP_DURATION
 cd "${CODEBASE_LOCATION}"
 
-TASK_STATUS=0
 DOCKER_FILE_PARENT_PATH=`getDockerfileParentPath`
 DOCKER_FILE_NAME=`getDockerfileName`
 
@@ -23,6 +22,22 @@ result=`getLineForAString ${DOCKERFILE_PATH} FROM`
 
 logInfoMessage "Got below lines in Dockerfile [${result}]"
 
-TASK_STATUS=`textExistsInALine "$result" "${WHITELIST_IMAGE_NAME}"`
+base_image=""
+# Consider the Dockerfiles with multi-stage builds.
+# In this case, the last one is considered as base image.
+while IFS= read -r line; do
+    base_image=$(echo "$line" | grep '^FROM ' | awk '{print $2}')
+done <<< "${result}"
+
+if [ -n "$base_image" ]; then
+    logInfoMessage "The base image is: ${base_image}"
+    textExistsInALine "$base_image" "${WHITELIST_IMAGE_NAME}"
+    if [ $? -eq 0 ]; then
+        logInfoMessage "Image is whitelisted: $base_image"
+    else
+        logWarningMessage "Image is not whitelisted: $base_image"
+        TASK_STATUS=1
+    fi
+fi
 
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
