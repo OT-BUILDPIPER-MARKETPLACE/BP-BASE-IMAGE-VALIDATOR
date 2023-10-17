7e29e0bbb343ddcfc4f04fa898d162000f3797c6
Rishabh <rishabhbohra01@gmail.com>
2023-10-17 22:32:47 +0530

    util: use function to extract base image
    Uses getBaseImageFromFilteredDockerfile function from the docker-functions.sh helper
    to extract the actual base image. Also rename WHITELIST_IMAGE_NAME variable name.

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
 source /opt/buildpiper/shell-functions/str-functions.sh
 source /opt/buildpiper/shell-functions/file-functions.sh
+source /opt/buildpiper/shell-functions/docker-functions.sh
 
 TASK_STATUS=0
 
@@ -15,23 +16,20 @@ DOCKER_FILE_PARENT_PATH=`getDockerfileParentPath`
 DOCKER_FILE_NAME=`getDockerfileName`
 
 DOCKERFILE_PATH="${DOCKER_FILE_PARENT_PATH}/${DOCKER_FILE_NAME}"
-logInfoMessage "Finding [${WHITELIST_IMAGE_NAME}] text in ${DOCKERFILE_PATH}"
+logInfoMessage "Finding [${WHITELIST_IMAGES_NAME}] text in ${DOCKERFILE_PATH}"
 cat ${DOCKERFILE_PATH}
 echo ""
 result=`getLineForAString ${DOCKERFILE_PATH} FROM`
 
 logInfoMessage "Got below lines in Dockerfile [${result}]"
 
-base_image=""
 # Consider the Dockerfiles with multi-stage builds.
 # In this case, the last one is considered as base image.
-while IFS= read -r line; do
-    base_image=$(echo "$line" | grep '^FROM ' | awk '{print $2}')
-done <<< "${result}"
+base_image=`getBaseImageFromFilteredDockerfile ${result}`
 
 if [ -n "$base_image" ]; then
     logInfoMessage "The base image is: ${base_image}"
-    textExistsInALine "$base_image" "${WHITELIST_IMAGE_NAME}"
+    textExistsInALine "$base_image" "${WHITELIST_IMAGES_NAME}"
     if [ $? -eq 0 ]; then
         logInfoMessage "Image is whitelisted: $base_image"
     else
