71960b78caeb63c50cfe1dffa5d8f7ef615c7ac0
Aayush Verma <aayush.verma@opstree.com>
2026-07-23 22:32:26 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -64,6 +64,8 @@ if [ -z "$WHITELIST_IMAGES_NAME" ]; then
     exit 1
 fi
 
+#WHITELIST_IMAGES_NAME="ubuntu:24.04,alpine:latest,node:20,python:3.12"
+
 if [ -n "$base_image" && -n "$WHITELIST_IMAGES_NAME" ]; then
     logInfoMessage "The base image is: ${base_image}"
     textExistsInALine "$base_image" "${WHITELIST_IMAGES_NAME}"
